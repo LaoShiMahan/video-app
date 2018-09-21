ac10e532ebd87b915c2985a48da79b91225eadf1
Tenyson Partridge <tenysonpartridge@gmail.com>
2018-09-21 14:58:17 -0600

    added navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Video from './video';
 import ReactVideos from './reactVideos';
+import Navbar from './navbar';
 
 export default class App extends Component {
     render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
         <div className='app'>
             <h1>Video App</h1>
             <Video />
+            <Navbar/>
         </div>
         );
     }
